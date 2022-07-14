afa4debf51cd411d4e9637451ad4b68691b23144
Felix Fernando <felixzhuang1@gmail.com>
2022-07-14 07:52:05 +0700

    first commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def data(): 
-    return "hello world"
+peoples = {
+    "name" : [] 
+}
+
+@app.route('/api', methods=["GET"])
+def get_peoples():
+    return peoples
+
+@app.route('/api/<name>', methods=["POST"])
+def add_peoples(name):
+    if name in peoples["name"]:
+        return peoples
+    peoples["name"].append(name)
+    return peoples
+
+@app.route('/api/<name>', methods=["DELETE"])
+def delete_peoples(name):
+    if name not in peoples["name"]:
+        return peoples
+    peoples["name"].remove(name)
+    return peoples
+
--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
 
 function App() {
+    const [ data, setData ] = useState()
+    const [ value, setValue ] = useState('')
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/api").then((res) => {
+            setData(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    const addUser = () => {
+        axios.post("http://localhost:5000/api/"+value).then((res) => {
+            setData(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+    const deleteUser = () => {
+        axios.delete("http://localhost:5000/api/"+value).then((res) => {
+            setData(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    if (typeof data === "undefined") return <h1>Loading...</h1>
+    
     return (
-        <div>App</div>
+        <div>
+            <h1>Name List</h1>
+            <div className='form'>
+                <input type="text" onChange={(e) => setValue(e.target.value)}></input>
+                <button onClick={() => addUser() }>Add</button>
+                <button onClick={() => deleteUser() }>Delete</button>
+            </div>
+            <ul>
+                {data.name.map((data, i) => {
+                    return (<li key={i}>{data}</li>)
+                })}
+            </ul>
+        </div>
     )
 }
 
